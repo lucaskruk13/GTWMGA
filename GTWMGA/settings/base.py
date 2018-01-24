221fd62c22b5828c353e9070476ea6624f452f11
Lucas Kruk <lucaskruk13@gmail.com>
2018-01-23 22:29:27 -0600

    Update requirmeents.txt

--- GTWMGA/settings/base.py
+++ GTWMGA/settings/base.py
@@ -14,7 +14,8 @@ import os
 import settings_secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -74,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'GTWMGA.wsgi.application'
+WSGI_APPLICATION = 'src.wsgi.application'
 
 
 # Database
@@ -137,3 +138,13 @@ STATIC_URL = '/static/'
 ROOT_URL = '/home/'
 AUTH_USER_MODEL = 'accounts.MyUser'
 LOGIN_REDIRECT_URL = '/home/'
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
---
+++ settings_secret.py
@@ -0,0 +1,4 @@
+username = 'gtwmga'
+password = 'M@@$@Kruk13!'
+host = 'localhost'
+dbname = 'gtw_mga'
